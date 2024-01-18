857b7af781c405da5f65e1f67c5dad8e85f778b2
jinujayachandran <jinujayachandran@gmail.com>
2024-01-18 20:27:03 +0530

    Script and output for cpu usage

---
+++ getRemote.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+import paramiko
+import warnings 
+import time
+from datetime import date
+
+output_file = 'paramiko.org' 
+warnings.filterwarnings(action='ignore',module='.*paramiko.*')
+def paramiko_GKG(hostname, command):
+    #print('running')
+    try:
+        port = '22'
+         
+        # created client using paramiko
+        client = paramiko.SSHClient()
+         
+        # here we are loading the system 
+        # host keys
+        client.load_system_host_keys()
+         
+        # connecting paramiko using host 
+        # name and password
+        client.connect(hostname, port=22, username='dell',
+                       password='dell')
+         
+        # below line command will actually 
+        # execute in your remote machine
+        (stdin, stdout, stderr) = client.exec_command(command,timeout=10)
+        exit_status = stdout.channel.recv_exit_status()          # Blocking call
+         
+        # redirecting all the output in cmd_output 
+        # variable
+        linenum=0
+        for line in stdout.readlines():
+                linenum = linenum+1
+                if(linenum == 2):
+                        out = line.strip()
+                        print(out)
+             
+        # we are returning the output
+        return out
+    finally:
+        client.close()
+ 
+#cmd ="Get-Counter -Counter '\Processor(_Total)\% Processor Time'"
+psCmd = "wmic cpu get loadpercentage"
+f = open("cpu_usage.txt", "w")
+
+#f.write("   IP Address            CPU Load    ")
+#f.write("------------------    ---------------")
+while(1):
+        t = time.localtime()
+        today = date.today()
+        d1 = today.strftime("%d-%m-%Y")
+        current_time = time.strftime("%H:%M:%S", t)
+        f.seek(0)
+
+        usage = paramiko_GKG('10.208.67.109', psCmd)
+        fstr ="10.208.67.109      "+(usage)+"%\n"
+        print(fstr)
+        f.write(fstr)
+
+        #usage = paramiko_GKG('10.208.67.26', psCmd)
+        #fstr ="10.208.67.26       "+(usage)+"%\n"
+        #print(fstr)
+        #f.write(fstr)
+
+        usage = paramiko_GKG('10.208.67.247', psCmd)
+        fstr ="10.208.67.247      "+(usage)+"%\n"
+        print(fstr)
+        f.write(fstr)
+
+        usage = paramiko_GKG('10.208.66.95', psCmd)
+        fstr ="10.208.66.95       "+(usage)+"%\n"
+        print(fstr)
+        f.write(fstr)
+
+        usage = paramiko_GKG('10.208.66.217', psCmd)
+        fstr ="10.208.66.217      "+(usage)+"%\n"
+        print(fstr)
+        f.write(fstr)
+
+        time.sleep(5)
+
+        f.write("\n\n\n")
+        timeStr="Updated On "+current_time+"  "+d1
+        f.write(timeStr)
+        f.truncate()
+
